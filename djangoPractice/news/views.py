bc920e4771e24b7ea0a5467cfc4ec51edc87687a
Yiknok <danyapavl20.03@gmail.com>
2022-09-02 18:23:17 +0300

    using class in views.py: ListView class 27

--- djangoPractice/news/views.py
+++ djangoPractice/news/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render, get_object_or_404,redirect
+from django.views.generic import ListView
 
 from .forms import NewsForm
 from .models import News, Category
+
+
+
+
+
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/class_index.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title']='Home page'
+        return context
+    def get_queryset(self):
+        return News.objects.filter(is_published = True)
+
 
 
 def index(request):
