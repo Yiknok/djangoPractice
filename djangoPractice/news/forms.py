dd7fdab737337c570b37f107a2b1daa826cf1f5d
Yiknok <danyapavl20.03@gmail.com>
2022-09-02 15:20:38 +0300

    use bound form with model

--- djangoPractice/news/forms.py
+++ djangoPractice/news/forms.py
@@ -1,24 +1,40 @@
 from django import forms
+from .models import News
 
-from .models import Category
 
-
-class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150, label='News title', widget=forms.TextInput(
-        attrs={
-            'class': 'form-control',
-        }))
-    content = forms.CharField(label='News content', required=False, widget=forms.Textarea(
-        attrs={
-            'class': 'form-control',
-            'rows': 5,
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5,}),
+            'category': forms.Select( attrs={'class': 'form-control',}),
         }
-    ))
-    is_published = forms.BooleanField(label='News status', initial=True)
-    category = forms.ModelChoiceField(empty_label='Choose news category', label='News category',
-                                      queryset=Category.objects.all(), widget=forms.Select(
-            attrs={
-                'class': 'form-control',
 
-            }
-        ))
+
+
+
+
+# from .models import Category
+#
+#
+# class NewsForm(forms.Form):
+#     title = forms.CharField(max_length=150, label='News title', widget=forms.TextInput(
+#         attrs={
+#             'class': 'form-control',
+#         }))
+#     content = forms.CharField(label='News content', required=False, widget=forms.Textarea(
+#         attrs={
+#             'class': 'form-control',
+#             'rows': 5,
+#         }
+#     ))
+#     is_published = forms.BooleanField(label='News status', initial=True)
+#     category = forms.ModelChoiceField(empty_label='Choose news category', label='News category',
+#                                       queryset=Category.objects.all(), widget=forms.Select(
+#             attrs={
+#                 'class': 'form-control',
+#
+#             }
+#         ))
--- djangoPractice/news/models.py
+++ djangoPractice/news/models.py
@@ -9,7 +9,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Last Update:')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True, verbose_name='Photo')
     is_published=models.BooleanField(default=True,verbose_name='Published')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='News Category')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='News Category')
 
     def get_absolute_url(self):
         return reverse('view_news',kwargs={'news_id':self.pk})
