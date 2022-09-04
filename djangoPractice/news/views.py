95cc3e3bf7554d54fcab83988f5e473734a80dc2
Yiknok <danyapavl20.03@gmail.com>
2022-09-04 20:10:19 +0300

    replace view func to DetailView class

--- djangoPractice/news/views.py
+++ djangoPractice/news/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render, get_object_or_404,redirect
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DeleteView
 
 from .forms import NewsForm
 from .models import News, Category
-
-
-
 
 
 class HomeNews(ListView):
@@ -15,11 +12,11 @@ class HomeNews(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title']='Home page'
+        context['title'] = 'Home page'
         return context
-    def get_queryset(self):
-        return News.objects.filter(is_published = True)
 
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
 
 
 class CategoryNews(ListView):
@@ -30,11 +27,18 @@ class CategoryNews(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title']=Category.objects.get(pk=self.kwargs['category_id'])
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
         return context
 
     def get_queryset(self):
-        return News.objects.filter(category_id=self.kwargs['category_id'],is_published = True )
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
+
+
+class ViewNews(DeleteView):
+    model = News
+    context_object_name = 'news_item'
+    # template_name = 'news/news_confirm_delete.html'
+    # pk_url_kwarg = 'news_id'
 
 
 # def index(request):
@@ -46,21 +50,21 @@ class CategoryNews(ListView):
 #     return render(request, 'news/index.html', context)
 
 
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'category': category})
+# def get_category(request, category_id):
+#     news = News.objects.filter(category_id=category_id)
+#     category = Category.objects.get(pk=category_id)
+#     return render(request, 'news/category.html', {'news': news, 'category': category})
 
 
-def view_news(request, news_id):
-    # news_item = News.objects.get(pk=news_id)
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(request, 'news/view_news.html', {'news_item': news_item})
+# def view_news(request, news_id):
+#     # news_item = News.objects.get(pk=news_id)
+#     news_item = get_object_or_404(News, pk=news_id)
+#     return render(request, 'news/view_news.html', {'news_item': news_item})
 
 
 def add_news(request):
     if request.method == 'POST':
-        form=NewsForm(request.POST)
+        form = NewsForm(request.POST)
         if form.is_valid():
             news = form.save()
             return redirect(news)
