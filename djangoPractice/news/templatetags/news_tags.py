8314f7bbf9f98995d32165e10d4d4b59fd2959e9
Yiknok <danyapavl20.03@gmail.com>
2022-09-07 00:31:16 +0300

    some changes when practice in django ORM

--- djangoPractice/news/templatetags/news_tags.py
+++ djangoPractice/news/templatetags/news_tags.py
@@ -1,14 +1,17 @@
 from django import template
+from django.db.models import Count
 
 from news.models import Category
 
 register = template.Library()
 
+
 @register.simple_tag()
 def get_categories():
     return Category.objects.all()
 
+
 @register.inclusion_tag('news/list_categories.html')
-def show_categories(arg1,arg2):
-    categories=Category.objects.all()
-    return {'categories':categories,'arg1':arg1,'arg2':arg2}
+def show_categories(arg1, arg2):
+    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
+    return {'categories': categories, 'arg1': arg1, 'arg2': arg2}
